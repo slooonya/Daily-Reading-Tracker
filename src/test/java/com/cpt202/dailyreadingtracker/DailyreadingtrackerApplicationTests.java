1c2cb5984d4d6801c5513d4165f647e8dd42f078
Slooonya <snmmnva@gmail.com>
2025-07-24 00:36:53 +0300

    Project setup + Account related entities + Authentication page layout

---
+++ src/test/java/com/cpt202/dailyreadingtracker/DailyreadingtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpt202.dailyreadingtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyreadingtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
